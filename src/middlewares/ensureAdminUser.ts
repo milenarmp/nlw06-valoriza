132cc9735591de6ebc449773941bb06545ee33d5
milenarmp <milenarmp@gmail.com>
2021-07-25 15:28:13 -0300

    feat: add ensureAdminUser middleware.

---
+++ src/middlewares/ensureAdminUser.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+export function ensureAdminUser(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const admin = true;
+
+  if (admin) {
+    return next();
+  }
+
+  return response.status(401).json({
+    error: "Unauthorized",
+  });
+}
