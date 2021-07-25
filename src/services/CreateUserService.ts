9e7eb40b98e4b755a91b1e05c58a4dc925a957ff
milenarmp <milenarmp@gmail.com>
2021-07-25 15:13:48 -0300

    refactor: error message when e-mail is not setted.

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -12,7 +12,7 @@ class CreateUserService {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if (!email) {
-      throw new Error("Email incorrect");
+      throw new Error("Incorrect e-mail");
     }
 
     const userAlreadyExists = await usersRepository.findOne({
