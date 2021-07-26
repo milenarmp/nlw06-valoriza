46a5f4a4f43924957053b7bb31ce2136d4891413
milenarmp <milenarmp@gmail.com>
2021-07-26 19:48:48 -0300

    feat: list tags and add class transformer to show name_custom.

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,25 +1,39 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { Expose } from "class-transformer";
+
 import { v4 as uuid } from "uuid";
 
 @Entity("tags")
 class Tag {
-    @PrimaryColumn()
-    readonly id: string;
+  @PrimaryColumn()
+  readonly id: string;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 
-    constructor() {
-        if(!this.id) {
-            this.id = uuid();
-        }
+  @Expose({name: "name_custom"})
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
     }
+  }
 }
 
 export { Tag };
